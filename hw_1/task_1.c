fb29f7e9d6bbc2fe03dd8e68e3084b3423b07283
Kilill_Shugay <shugaykirilll@gmail.com>
2023-10-31 01:03:15 +0200

    hw_1

---
+++ hw_1/task_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //Задание 1. Вывести на экран “Hello world!”
+    printf("task 1.\nHello, World!\n");
+    // Задание 2. Вывести на экран текст "лесенкой"
+    printf("task 2.\n Let`s\n\t go\n\t\t to walk\n");
+    // Задание 3. Вывести на экран рисунок из букв
+    printf("task 3.\n  *   \n ***\n***** \nHH HH\nZZZZZ\n");
+    // Задание 4*. Нарисовать соты
+    printf("task 4.\n _   _\n/ \\_/ \\\n\\_/ \\_/\n/ \\_/ \\\n\\_/ \\_/\n");
+    return 0;
+}
